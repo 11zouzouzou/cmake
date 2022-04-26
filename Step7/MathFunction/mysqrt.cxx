56fac3ca122e918c7d6c9b475a5fcd70972d2332
zouzouzou11 <1096174991@qq.com>
2022-04-26 18:11:28 +0800

    feat: add step7

--- Step7/MathFunction/mysqrt.cxx
+++ Step7/MathFunction/mysqrt.cxx
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include "MathFunctions.h"
+#include "Table.h"
 
 using namespace std;
 double mysqrt(double x)
@@ -9,14 +10,16 @@ double mysqrt(double x)
     {
         return 0;
     }
-    // if we have both log and exp then use them
-#if defined(HAVE_LOG) && defined(HAVE_EXP)
-    double result = exp(log(x) * 0.5); // need include cmath
-    std::cout << "Computing sqrt of " << x << " to be " << result
-              << " using log and exp" << std::endl;
-#else
 
     double result = x;
+
+    if (x >= 1 && x < 10)
+    {
+        std::cout << "Use the table to help find an initial value " << std::endl;
+        result = sqrtTable[static_cast<int>(x)];
+        std::cout << "table value: " << result << std::endl;
+    }
+
     for (int i = 0; i < 10; i++)
     {
         if (result <= 0)
@@ -28,6 +31,5 @@ double mysqrt(double x)
 
         std::cout << "computing sqrt og" << x << "to be" << result << std::endl;
     }
-#endif
     return result;
 }
